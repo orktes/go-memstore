cc1141a967e0ba46560f55ad51d0fce223ae8ca9
Jaakko Lukkari <jaakko@unity3d.com>
2018-03-02 16:57:58 +0200

    Simple test

--- examples/simple_test.go
+++ examples/simple_test.go
@@ -1,6 +1,9 @@
 package examples
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+)
 
 func TestSimpleStructMemStore(t *testing.T) {
 	store := NewSimpleStructMemStore()
@@ -24,5 +27,25 @@ func TestSimpleStructMemStore(t *testing.T) {
 
 	if _, ok := store.Get(SimpleStructMemStoreQuery{Foo: 1, Bar: 1}); ok {
 		t.Error("Should not return record which has not been inserted")
+	}
+}
+
+func BenchmarkSimpleStructMemStore(b *testing.B) {
+	store := NewSimpleStructMemStore()
+	rand.Seed(42)
+
+	for i := 0; i < b.N; i++ {
+		insert := SimpleStruct{
+			Foo: rand.Intn(b.N),
+			Bar: rand.Intn(b.N),
+			Biz: "foobarbiz",
+			Val: 0.56,
+		}
+		store.Insert(insert)
+
+		_, ok := store.Get(SimpleStructMemStoreQuery{Foo: insert.Foo, Bar: insert.Bar})
+		if !ok {
+			b.Error("No result found for query")
+		}
 	}
 }
